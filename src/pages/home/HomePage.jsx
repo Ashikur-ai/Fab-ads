34b143ffdd6029ec2a425572b6db150f4e3a8501
ashikur-ai <projectshortlife@gmail.com>
2025-02-14 22:28:12 +0600

    payment section done

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../components/homepage/Banner';
+import Payments from '../../components/homepage/Payments';
 
 const HomePage = () => {
   return (
     <div>
-      <Banner/>
+      <Banner />
+      <Payments/>
     </div>
   );
 };
